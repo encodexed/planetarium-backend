0fa0d04f5664cf1cb851a8d9d1c2b714378ead13
Robbie Gollan <robbiegollan@gmail.com>
2023-11-27 14:38:55 +1100

    first commit

---
+++ src/main/java/com/planetarium/planetarium/PlanetariumApplication.java
@@ -0,0 +1,13 @@
+package com.planetarium.planetarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetariumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planetarium/planetarium/PlanetariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planetarium.planetarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
