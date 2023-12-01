d8025186b0705640d6829d2c8510d7282d83a890
Robbie Gollan <robbiegollan@gmail.com>
2023-12-01 18:13:11 +1100

    adds data-seeding setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+  users(id, email, password, role, username)
+VALUES
+  (1, "admin@test.com", "password", "ROLE_ADMIN", "admin"),
+  (2, "robbie@test.com", "password", "ROLE_ADMIN", "robbie")
+;
