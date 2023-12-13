c1d8c7e34e9083a2b9336e79da60d683b2dd1f06
Robbie Gollan <robbiegollan@gmail.com>
2023-12-13 14:10:18 +1100

    adds star put route - star system relational mapping still not quite working as intended

--- src/main/java/com/planetarium/planetarium/stars/StarController.java
+++ src/main/java/com/planetarium/planetarium/stars/StarController.java
@@ -3,12 +3,14 @@ package com.planetarium.planetarium.stars;
 import com.planetarium.planetarium.exceptions.NotFoundException;
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +52,24 @@ public class StarController {
   ) {
     Star newStar = this.starService.createStar(data);
     return new ResponseEntity<Star>(newStar, HttpStatus.CREATED);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Star> updateStar(
+    @PathVariable Long id,
+    @RequestBody UpdateStarDTO data
+  ) {
+    Optional<Star> replacedStar = this.starService.replaceById(id, data);
+
+    if (replacedStar.isPresent()) {
+      return new ResponseEntity<>(replacedStar.get(), HttpStatus.OK);
+    }
+
+    throw new NotFoundException(
+      String.format(
+        "Could not update Star with id: %d because it does not exist",
+        id
+      )
+    );
   }
 }
